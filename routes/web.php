eabaa607b22a44419756277b6abe919e65112eba
semih-76 <semihsisman7@gmail.com>
2026-07-21 15:44:33 +0200

    configure root

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ResourceController;
 
+
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/resources', [ResourceController::class, 'index']);
-Route::get('/resource/{resource}', [ResourceController::class, 'show']);
+Route::resource('resources', ResourceController::class);
