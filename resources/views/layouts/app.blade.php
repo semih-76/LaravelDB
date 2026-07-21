a70d681805ccebe01c1f4bd8eb8fe05e0e832e37
semih-76 <semihsisman7@gmail.com>
2026-07-21 15:50:03 +0200

    configure message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,31 @@
     <title>@yield('title', 'Médiathèque')</title>
 </head>
 <body>
+
+@if(session('success'))
+    <div style="background:green; color:white; padding:10px;">
+        {{ session('success') }}
+    </div>
+@endif
+
+@if(session('info'))
+    <div>
+        {{ session('success') }}
+    </div>
+@endif
+
+    <ul>
+        @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+
+@if ($errors->any())
+
+@endif
 @yield('content')
 </body>
 </html>
+
+
+
