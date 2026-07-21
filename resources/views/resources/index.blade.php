8bf6008d8c9a6b70309cddfcdb417141f6a8a5e2
semih-76 <semihsisman7@gmail.com>
2026-07-21 15:46:14 +0200

    configure index

--- resources/views/resources/index.blade.php
+++ resources/views/resources/index.blade.php
@@ -4,12 +4,20 @@
 
 @section('content')
     <h1>Ressources</h1>
+    <a href="{{ route('resources.create') }}">Ajouter</a>
+
     @foreach($resources as $resource)
         <div>
-            <a href="{{url('/resource/' . $resource['id']) }}">
-                {{ $resource['title'] }}
+            <a href="{{ route('resources.show', $resource->id) }}">
+                {{ $resource->titre }}
             </a>
-            <p>{{ $resource['description'] }}</p>
+            <p>{{ $resource->type }} - {{ $resource->statut }}</p>
+            <a href="{{ route('resources.edit', $resource->id) }}">Modifier</a>
+            <form method="POST" action="{{ route('resources.destroy', $resource->id) }}" style="display:inline">
+                @csrf
+                @method('DELETE')
+                <button type="submit" onclick="return confirm('Supprimer cette ressource ?')">Supprimer</button>
+            </form>
         </div>
-        @endforeach
-    @endsection
+    @endforeach
+@endsection
