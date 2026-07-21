55a011222c3acbfa459d908578907d5c25e7a6b9
semih-76 <semihsisman7@gmail.com>
2026-07-21 15:47:53 +0200

    configure ResourceFactory

--- resources/views/resources/show.blade.php
+++ resources/views/resources/show.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.app')
 
-@section('title', $resource['title'])
+@section('title', $resource->titre)
 
 @section('content')
-    <h1>{{ $resource['title'] }}</h1>
-    <p>{{ $resource['description']}}</p>
-    <a href="{{ url('/resources') }}"><- Retour</a>
+    <h1>{{ $resource->titre }}</h1>
+
+    <p><strong>Type :</strong> {{ $resource->type }}</p>
+    <p><strong>Statut :</strong> {{ $resource->statut }}</p>
+    <p><strong>Emprunteur :</strong> {{ $resource->emprunteur ?? 'Aucun' }}</p>
+    <p><strong>Ajouté le :</strong> {{ $resource->created_at->format('d/m/Y') }}</p>
+
+    <a href="{{ route('resources.edit', $resource->id) }}">Modifier</a>
+
+    <form method="POST" action="{{ route('resources.destroy', $resource->id) }}" style="display:inline">
+        @csrf
+        @method('DELETE')
+        <button type="submit" onclick="return confirm('Supprimer cette ressource ?')">Supprimer</button>
+    </form>
+
+    <a href="{{ route('resources.index') }}">Retour à la liste</a>
 @endsection
 
